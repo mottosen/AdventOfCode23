cd45b138bd8bb4f48bd63e1027660d1110498db9
mottosen <subscribe@mottosen.xyz>
2023-12-02 19:58:54 +0100

    cleanup.

--- AdventOfCode/Day1.fs
+++ AdventOfCode/Day1.fs
@@ -4,23 +4,8 @@ open System.Text.RegularExpressions
 
 type Day1() =
     static member private line2int (line : string) : int =
-        let ints = Regex.Matches(line, "\d")
-
-        if (ints.Count = 0) then 0
-        else
-            Array.init (ints.Count) (fun i -> ints[i].Value)
-            |> fun arr -> int (Array.head arr + Array.last arr)
-
-    static member private fixLine (line : string) : string =
-        Regex.Replace(line, "one", "o1ne")
-        |> fun l -> Regex.Replace(l, "two", "t2wo")
-        |> fun l -> Regex.Replace(l, "three", "t3hree")
-        |> fun l -> Regex.Replace(l, "four", "f4our")
-        |> fun l -> Regex.Replace(l, "five", "f5ive")
-        |> fun l -> Regex.Replace(l, "six", "s6ix")
-        |> fun l -> Regex.Replace(l, "seven", "s7even")
-        |> fun l -> Regex.Replace(l, "eight", "e8ight")
-        |> fun l -> Regex.Replace(l, "nine", "n9ine")
+        Regex.Matches(line, "\d")
+        |> fun seq -> int ((Seq.head seq).Value + (Seq.last seq).Value)
 
     static member Star1 (input : string[]) : string =
         (0, input)
@@ -28,6 +13,17 @@ type Day1() =
         |> string
 
     static member Star2 (input : string[]) : string =
+        let fixLine (line : string) : string =
+            Regex.Replace(line, "one", "o1ne")
+            |> fun l -> Regex.Replace(l, "two", "t2wo")
+            |> fun l -> Regex.Replace(l, "three", "t3hree")
+            |> fun l -> Regex.Replace(l, "four", "f4our")
+            |> fun l -> Regex.Replace(l, "five", "f5ive")
+            |> fun l -> Regex.Replace(l, "six", "s6ix")
+            |> fun l -> Regex.Replace(l, "seven", "s7even")
+            |> fun l -> Regex.Replace(l, "eight", "e8ight")
+            |> fun l -> Regex.Replace(l, "nine", "n9ine")
+
         (0, input)
-        ||> Array.fold (fun acc s -> acc + (s |> Day1.fixLine |> Day1.line2int))
+        ||> Array.fold (fun acc s -> acc + (s |> fixLine |> Day1.line2int))
         |> string
