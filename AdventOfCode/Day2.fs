97bab030dc4cf6fd5284aece18046d48683dc5b1
mottosen <subscribe@mottosen.xyz>
2023-12-02 19:58:54 +0100

    cleanup.

--- AdventOfCode/Day2.fs
+++ AdventOfCode/Day2.fs
@@ -5,20 +5,16 @@ open System.Text.RegularExpressions
 
 type Day2() =
     static member private handleSet (set : string) : int*int*int =
-        let cubes = Regex.Matches(set, "(\d+) (blue|red|green)")
+        let cubes : Match seq = Regex.Matches(set, "(\d+) (blue|red|green)") |> Seq.cast
 
-        if cubes.Count = 0 then (0,0,0)
-        else
-            let arr = Array.init (cubes.Count) (fun i -> cubes[i])
-
-            ((0,0,0), arr)
-            ||> Array.fold (fun (r,g,b) a -> 
-                match a.Groups[2].Value with
-                | "red" -> (Math.Max(r, a.Groups[1].Value |> int), g, b)
-                | "green" -> (r, Math.Max(g, a.Groups[1].Value |> int), b)
-                | "blue" -> (r, g, Math.Max(b, a.Groups[1].Value |> int))
-                | _ -> (r,g,b)
-            )
+        ((0,0,0), cubes)
+        ||> Seq.fold (fun (r,g,b) a -> 
+            match a.Groups[2].Value with
+            | "red" -> (Math.Max(r, a.Groups[1].Value |> int), g, b)
+            | "green" -> (r, Math.Max(g, a.Groups[1].Value |> int), b)
+            | "blue" -> (r, g, Math.Max(b, a.Groups[1].Value |> int))
+            | _ -> (r,g,b)
+        )
 
     static member Star1 (input : string[]) : string =
         let (red_limit,green_limit,blue_limit) = 12,13,14
@@ -28,14 +24,11 @@ type Day2() =
 
             let sets = Regex.Matches(game, "(((\d+) (blue|red|green)),? ?)+")
 
-            if sets.Count = 0 then 0
-            else
-                let validGame =
-                    Array.init (sets.Count) (fun i -> sets[i].Value)
-                    |> Array.map Day2.handleSet
-                    |> Array.forall (fun (r,g,b) -> r <= red_limit && g <= green_limit && b <= blue_limit)
+            let validGame =
+                Seq.map (fun (x : Match) -> Day2.handleSet x.Value) sets
+                |> Seq.forall (fun (r,g,b) -> r <= red_limit && g <= green_limit && b <= blue_limit)
 
-                if validGame then gameID else 0
+            if validGame then gameID else 0
 
         (0, input)
         ||> Array.fold (fun acc game -> acc + (handleGame game))
@@ -45,14 +38,11 @@ type Day2() =
         let handleGame (game : string) : int =
             let sets = Regex.Matches(game, "(((\d+) (blue|red|green)),? ?)+")
 
-            if sets.Count = 0 then 0
-            else
-                let (r,g,b) =
-                    Array.init (sets.Count) (fun i -> sets[i].Value)
-                    |> Array.map Day2.handleSet
-                    |> Array.fold (fun (ra,ga,ba) (r,g,b) -> (Math.Max(ra, r), Math.Max(ga, g), Math.Max(ba, b))) (0,0,0)
+            let (r,g,b) =
+                Seq.map (fun (x : Match) -> Day2.handleSet x.Value) sets
+                |> Seq.fold (fun (ra,ga,ba) (r,g,b) -> (Math.Max(ra, r), Math.Max(ga, g), Math.Max(ba, b))) (0,0,0)
 
-                r*g*b
+            r*g*b
 
         (0, input)
         ||> Array.fold (fun acc game -> acc + (handleGame game))
