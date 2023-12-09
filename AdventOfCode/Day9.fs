14d5c025647e5094ee0329e93c5b720ff48d259e
mottosen <subscribe@mottosen.xyz>
2023-12-09 16:14:01 +0100

    minor cleanup.

--- AdventOfCode/Day9.fs
+++ AdventOfCode/Day9.fs
@@ -1,20 +1,18 @@
 namespace AdventOfCode
 
 type Day9() =
-    static member private line2num (line : int list) : int =
-        let rec helper (diffs : int list) (line : int list) : int =
-            if diffs.Length = 0 && (line |> List.forall ((=) 0)) then 0
+    static member private line2num : int list -> int =
+        let rec helper (res : int) (diffs : int list) (line : int list) : int =
+            if diffs.Length = 0 && (line |> List.forall ((=) 0)) then res
             else match line with
-                 | e1 :: e2 :: r -> let diff = e2 - e1 in helper (diff :: diffs) (e2 :: r)
-                 | [e] -> e + (helper [] (diffs |> List.rev))
-        helper [] line
+                 | e1 :: e2 :: r -> helper res ((e2 - e1) :: diffs) (e2 :: r)
+                 | [e] -> helper (res + e) [] (diffs |> List.rev)
+        helper 0 []
 
-    static member Star1 (input : string[]) : string =
-        input
-        |> Array.map (fun line -> line.Split(' ') |> Array.map (fun n -> int n) |> fun arr -> Day9.line2num (arr |> List.ofArray))
-        |> Array.sum |> string
+    static member Star1 : string[] -> string =
+        Array.map (fun line -> line.Split(' ') |> Array.map int |> List.ofArray |> Day9.line2num)
+        >> Array.sum >> string
 
-    static member Star2 (input : string[]) : string =
-        input
-        |> Array.map (fun line -> line.Split(' ') |> Array.map (fun n -> int n) |> fun arr -> Day9.line2num (arr |> Array.rev |> List.ofArray))
-        |> Array.sum |> string
+    static member Star2 : string[] -> string =
+        Array.map (fun line -> line.Split(' ') |> Array.map int |> Array.rev |> List.ofArray |> Day9.line2num)
+        >> Array.sum >> string
