a3fa61be65c8dae5050da0ce3a398e35c10d50a8
mottosen <subscribe@mottosen.xyz>
2023-12-09 15:52:44 +0100

    Day 9 solutions.

--- AdventOfCode/Day9.fs
+++ AdventOfCode/Day9.fs
@@ -1,20 +1,20 @@
 namespace AdventOfCode
 
-open System
-
 type Day9() =
-    static member Star1 (input : string[]) : string =
-        let rec line2num (diffs : int list) (line : int list) : int =
+    static member private line2num (line : int list) : int =
+        let rec helper (diffs : int list) (line : int list) : int =
             if diffs.Length = 0 && (line |> List.forall ((=) 0)) then 0
-            else
-                match line with
-                | e1 :: e2 :: r -> let diff = e2 - e1 in line2num (diffs @ [diff]) (e2 :: r)
-                | [e] -> e + (line2num [] diffs)
+            else match line with
+                 | e1 :: e2 :: r -> let diff = e2 - e1 in helper (diffs @ [diff]) (e2 :: r)
+                 | [e] -> e + (helper [] diffs)
+        helper [] line
 
+    static member Star1 (input : string[]) : string =
         input
-        |> Array.map (fun line -> line.Split(' ') |> Array.map (fun n -> int n) |> fun arr -> line2num [] (List.ofArray arr))
-        |> Array.sum
-        |> string
+        |> Array.map (fun line -> line.Split(' ') |> Array.map (fun n -> int n) |> fun arr -> Day9.line2num (arr |> List.ofArray))
+        |> Array.sum |> string
 
     static member Star2 (input : string[]) : string =
-        raise (new NotImplementedException("Day9.Star2"))
+        input
+        |> Array.map (fun line -> line.Split(' ') |> Array.map (fun n -> int n) |> fun arr -> Day9.line2num (arr |> Array.rev |> List.ofArray))
+        |> Array.sum |> string
